3927da3f6eb68a860bca86c64a4c30f4c68e263e
heqingbao <qingbao.ho@gmail.com>
2019-03-24 17:36:13 +0800

    【分类】左侧导航栏

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,6 +1,6 @@
 const serviceUrl = 'http://test.baixingliangfan.cn/baixing/';
 const servicePath = {
   'homePageContent': serviceUrl + 'wxmini/homePageContent', // 商店首页信息
-  'homePageBelowContent':
-      serviceUrl + 'wxmini/homePageBelowConten', // 商城首页热卖商品
+  'homePageBelowContent': serviceUrl + 'wxmini/homePageBelowConten', // 商城首页热卖商品
+  'getCategory': serviceUrl + 'wxmini/getCategory', // 商品类别信息
 };
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:convert';
+import '../service/service_method.dart';
+import '../model/category.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('分类页面'),
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+// 左侧大类导航
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCategory();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: ScreenUtil().setWidth(180),
+        decoration: BoxDecoration(
+            border: Border(right: BorderSide(width: 1, color: Colors.black12))),
+        child: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return _leftInkWell(index);
+          },
+        ));
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10.0, top: 20.0),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border:
+                Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
+      ),
+    );
+  }
+
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
+      CategoryModel category = CategoryModel.fromJson(data);
+      // list.data.forEach((item) => print(item.mallCategoryName));
+      setState(() {
+        list = category.data;
+      });
+    });
   }
 }
